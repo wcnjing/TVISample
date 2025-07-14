44dd1e436c8f7f68f0f40fb57443361e67853953
wcnjing <s10242078@connect.np.edu.sg>
2025-07-14 20:31:45 +0800

    Added nav bar and front page

---
+++ script.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.style.transform = 'translateY(-100%)';
+        navbar.style.transition = 'transform 0.3s';
+    } else {
+        navbar.style.transform = 'translateY(0)';
+        navbar.style.transition = 'transform 0.3s';
+    }
+});
